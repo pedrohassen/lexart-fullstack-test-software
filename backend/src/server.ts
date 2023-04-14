129dafa92b1dcfb5a6392550c8b4b0c3ea916d03
pedrohassen <pedro_hasse@yahoo.com.br>
2023-04-14 20:59:10 -0300

    Fix server.ts to start server properly

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,10 @@
+import { SearchFactory } from "./api/factory/SearchFactory";
 import App from "./app";
 
+const searchFactory = SearchFactory.createSearchRouter();
 const server = new App();
 
+server.addRouter(searchFactory);
 server.startServer();
 
 export default server;
