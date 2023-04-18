3d9d7e4a84ef67968377133a3d120b5ad7184a82
pedrohassen <pedro_hasse@yahoo.com.br>
2023-04-17 21:20:04 -0300

    refactor: 💡 Change code from class to function
    Create search.controller.ts which returns backend response of both
    webpages scraps

--- backend/src/api/controllers/search.controller.ts
+++ backend/src/api/controllers/search.controller.ts
@@ -1,14 +1,33 @@
 import { type Request, type Response } from "express";
-import SearchService from "../services/search.service";
+import { saveSearchService } from "../services/search.service";
+import { meliScrapper } from "../scrappers/meli.scrapper";
+import { buscapeScrapper } from "../scrappers/buscape.scrapper";
 
-export default class SearchController {
-  constructor(private readonly service: SearchService) {}
+export const saveSearchController = async (req: Request, res: Response): Promise<Response> => {
+  const { photo, description, category, price, website } = req.body;   
 
-  readonly saveSearch = async (req: Request, res: Response): Promise<Response> => {
-    const { photo, description, category, price, website } = req.body;   
+  const search = await saveSearchService({ photo, description, category, price, website });
 
-    const search = await this.service.saveSearch({ photo, description, category, price, website });
+  return res.status(200).json(search);
+}
 
-    return res.status(201).json(search);
+export const queryController = async (req: Request, res: Response): Promise<Response> => {
+  const { query, web, category } = req.body;
+
+  if (!query || !web || !category) {
+    return res.status(400).json({ message: 'Missing parameters' });
   }
+
+  if (web === 'Mercado Livre') {
+    const scrapMeli = await meliScrapper(query);
+    return res.status(200).json(scrapMeli);
+  } if (web === 'Buscapé') {
+    const scrapBusca = await buscapeScrapper(query);
+    return res.status(200).json(scrapBusca);
+  }
+
+  const scrapMeli = await meliScrapper(query);
+  const scrapBusca = await buscapeScrapper(query);
+
+  return res.status(200).json([...scrapMeli, ...scrapBusca]);
 }
