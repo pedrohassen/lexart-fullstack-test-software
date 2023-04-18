311d7612f9232fd1024134a88cd7bc44a3e168c3
pedrohassen <pedro_hasse@yahoo.com.br>
2023-04-17 21:14:33 -0300

    refactor: 💡 Change code from class to function

--- backend/src/api/routes/search.route.ts
+++ backend/src/api/routes/search.route.ts
@@ -1,14 +1,7 @@
 import { Router } from "express";
-import type SearchesController from "../controllers/search.controller";
+import { queryController } from "../controllers/search.controller";
 
-export default class SearchRoute {
-  public _router: Router;
+const router = Router();
+router.post('/', queryController);
 
-  constructor(private readonly controller: SearchesController) {
-    this._router = Router();
-  }
-
-  public _routes = (route = '/search'): void =>{
-    this._router.post(route, this.controller.saveSearch);
-  }
-}
+export default router;
