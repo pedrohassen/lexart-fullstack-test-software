7a9747a0768efb3ddb1a83fdae5a52eb0c48081f
pedrohassen <pedro_hasse@yahoo.com.br>
2023-04-17 21:30:17 -0300

    feat: 🦾 Create button component

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = ({text, ...props}: any) => {
+  return (
+    <button
+      {...props}
+    >
+      {text}
+    </button>
+  )
+}
+
+export default Button
